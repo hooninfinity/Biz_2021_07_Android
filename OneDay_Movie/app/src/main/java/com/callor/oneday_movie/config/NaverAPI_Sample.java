bf47204734461c39a2047ae2af2a47222fa99e87
hooninfinity <hooninfinity@naver.com>
2021-08-13 09:36:24 +0900

    sample

---
+++ OneDay_Movie/app/src/main/java/com/callor/oneday_movie/config/NaverAPI_Sample.java
@@ -0,0 +1,12 @@
+package com.callor.oneday_movie.config;
+
+public class NaverAPI_Sample {
+
+    public static final String CLIENT_ID = "YOUR NAVER API ID";
+    public static final String CLIENT_SECRET = "YOUR NAVER API SECRET";
+
+    public static final String NAVER_BOOK_URL = "https://openapi.naver.com/v1/search/movie.json";
+    public static final String NAVER_BASE_URL = "https://openapi.naver.com/v1/search/";
+
+
+}
